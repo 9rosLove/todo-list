6406396ec07cad76c7d104902a1189345c3203a4
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-10-05 11:49:39 +0300

    Implement home page, CRUD views and urls

--- todo/models.py
+++ todo/models.py
@@ -5,7 +5,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=63)
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
@@ -16,10 +16,14 @@ class Task(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
-    tags = models.ManyToManyField(to=Tag, related_name='tasks')
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
 
     class Meta:
-        ordering = ['is_done', '-created_at']
+        ordering = ["is_done", "-created_at"]
 
     def __str__(self):
-        return self.content[:30] + '...' if len(self.content) > 30 else self.content
+        return (
+            self.content[:30] + "..."
+            if len(self.content) > 30
+            else self.content
+        )
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from todo.models import Task
+
+
+def index(request):
+    return render(request, "todo/index.html")
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+
+
+class TagListView(generic.ListView):
+    model = Task
+
+
+class TagCreateView(generic.CreateView):
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:tag-list")
